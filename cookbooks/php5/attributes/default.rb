1d72914d583e7e6d9b70365e16b37e0351697175
David Laing <david@davidlaing.com>
2011-11-11 16:25:01 -0800

    added php5 and phpadmin cookbooks

---
+++ cookbooks/php5/attributes/default.rb
@@ -0,0 +1,11 @@
+default[:php5][:module_list]        = ""
+default[:php5_fpm][:listen]         = "socket"
+default[:php5_fpm][:listen_socket]  = "/var/run/php5-fpm.sock"
+default[:php5_fpm][:listen_ip]      = "127.0.0.1"
+default[:php5_fpm][:listen_port]    = "9000"
+
+if attribute?(:ec2)
+  default[:php5][:fpm_log_dir]  = "/mnt/log/php5-fpm"
+else
+  default[:php5][:fpm_log_dir]  = "/var/log/php5-fpm"
+end
