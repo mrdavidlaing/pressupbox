7e2136368462e4bfb823380a29756bfed6f701e8
David Laing <david@davidlaing.com>
2012-02-25 19:52:08 +0000

    Update version

--- cookbooks/app_containers/metadata.rb
+++ cookbooks/app_containers/metadata.rb
@@ -1,4 +1,4 @@
 maintainer        "David Laing"
 maintainer_email  "david@davidlaing.com"
 description       "Configure hosting container for webapps"
-version           "0.1"
+version           "0.1.1"
