ec8c8a4f97e59288f14d44a7f55d28c90814690f
David Laing <david@davidlaing.com>
2012-04-26 15:36:01 +0100

    Set default file upload limit to 10mb in Nginx & PHP

--- cookbooks/nginx/attributes/default.rb
+++ cookbooks/nginx/attributes/default.rb
@@ -43,3 +43,4 @@ default[:nginx][:keepalive_timeout]  = 65
 default[:nginx][:worker_processes]   = cpu[:total]
 default[:nginx][:worker_connections] = 2048
 default[:nginx][:server_names_hash_bucket_size] = 64
+default[:nginx][:client_max_body_size] = "10m"
