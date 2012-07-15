2ae0cc32eb394972186adbe84c08fadcd12cf66b
David Laing <david@davidlaing.com>
2012-07-15 17:36:22 +0100

    Include git branch in prompt

--- cookbooks/bash_for_hipsters/metadata.rb
+++ cookbooks/bash_for_hipsters/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "david@davidlaing.com"
 license          "Apache v2"
 description      "Installs/Configures a slick bash shell configuration"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.0"
 
 recipe            "default", "Install helper scripts to /var/local/bash_for_hipsters"
