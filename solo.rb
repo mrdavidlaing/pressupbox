869289b4b2e1cdfb578e0939463a961710788372
David Laing <david@davidlaing.com>
2012-08-27 22:09:39 +0100

    Fix typo

--- solo.rb
+++ solo.rb
@@ -1,6 +1,6 @@
 current_dir = File.dirname(__FILE__)
 
 file_cache_path "/var/chef-solo"
-cookbook_path ["#{current_dir}/cookbooks","#{current_dir}/cookbook-overrides"]
+cookbook_path ["#{current_dir}/cookbooks","#{current_dir}/cookbooks-overrides"]
 data_bag_path "#{current_dir}/data_bags"
 json_attribs "#{current_dir}/node.json"
